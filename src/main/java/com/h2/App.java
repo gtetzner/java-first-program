da67b7964f0c65b1ed8d700c381b57ab85488518
gtetzner <gtetzner0@gmail.com>
2021-04-23 13:52:47 -0300

    teste

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,14 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers ) {
+        var sun = 0;
+        for(int number: numbers) {
+            sun += number;
+        }
+        return sun;
     }
 }
